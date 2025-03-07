f2baf91bae502e5465af449f7689ac39cd811f39
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2025-03-07 01:48:07 -0500

    small

--- js/scrollHeader.js
+++
@@ -1,7 +0,0 @@
-// scrollHeader.js
-export function scrollHeader() {
-    const header = document.getElementById('header');
-    if (window.scrollY >= 200) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
-}
-window.addEventListener('scroll' , scrollHeader)
