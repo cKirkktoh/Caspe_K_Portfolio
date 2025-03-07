4718bb2e36a2aef0ec4810e5197d8c6a24a3718e
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2025-03-07 01:59:15 -0500

    final changes

---
+++ js/hamburger.js
@@ -0,0 +1,22 @@
+/* show hamburger menu*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/* remove hamburger menu*/ 
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
